b7a7319bac2a8b8eac0479e8b51ea4337e53a909
dralexportfolio <dr.alex.portfolio@gmail.com>
2026-01-21 12:55:40 -0800

    Update catan_generator.py

--- catan_boards/catan_generator.py
+++ catan_boards/catan_generator.py
@@ -24,8 +24,115 @@ from Polygon import HEXAGON_REGULAR_TALL, Polygon
 #####################################################
 ### Define important shared settings for the game ###
 #####################################################
+# Define the lists needed for parameter selection
+ALL_GAME_MODES = ["Original: 3-4 Player", "Original: 5-6 Player", "Seafarers: 3-4 Player", "Seafarers: 5-6 Player"]
+
+# Define a dictionary of number counts for each game mode
+NUMBER_COUNTS_PER_MODE = {
+	"Original: 3-4 Player": {		# 20 total
+		2: 1,
+		3: 2,
+		4: 2,
+		5: 2,
+		6: 2,
+		8: 2,
+		9: 2,
+		10: 2,
+		11: 2,
+		12: 1
+	},
+	"Original: 5-6 Player": {		# 28 total
+		2: 2,
+		3: 3,
+		4: 3,
+		5: 3,
+		6: 3,
+		8: 3,
+		9: 3,
+		10: 3,
+		11: 3,
+		12: 2
+	},
+	"Seafarers: 3-4 Player": {		# 28 total
+		2: 2,
+		3: 3,
+		4: 3,
+		5: 3,
+		6: 3,
+		8: 3,
+		9: 3,
+		10: 3,
+		11: 3,
+		12: 2
+	},
+	"Seafarers: 5-6 Player": {		# 32 total
+		2: 2,
+		3: 3,
+		4: 4,
+		5: 4,
+		6: 3,
+		8: 3,
+		9: 4,
+		10: 4,
+		11: 3,
+		12: 2
+	}
+}
+
+# Define a dictionary of tile count per row for each game mode
+ROW_COUNTS_PER_MODE = {
+	"Original: 3-4 Player": [3, 4, 5, 4, 3],
+	"Original: 5-6 Player": [3, 4, 5, 6, 5, 4, 3],
+	"Seafarers: 3-4 Player": [5, 6, 7, 6, 7, 6, 5],
+	"Seafarers: 5-6 Player": [7, 8, 9, 8, 9, 8, 7]
+}
+
+# Define a dictionary of tile count per type for each game mode
+TILE_COUNTS_PER_MODE = {
+	"Original: 3-4 Player": {		# 19 total, 18 with numbers
+		"brick": 3,
+		"sheep": 4,
+		"stone": 3,
+		"wheat": 4,
+		"wood": 4,
+		"desert": 1,
+		"gold": 0,
+		"water": 0
+	},
+	"Original: 5-6 Player": {		# 30 total, 28 with numbers
+		"brick": 5,
+		"sheep": 6,
+		"stone": 5,
+		"wheat": 6,
+		"wood": 6,
+		"desert": 2,
+		"gold": 0,
+		"water": 0
+	},
+	"Seafarers: 3-4 Player": {		# 42 total, 25 with numbers
+		"brick": 4,
+		"sheep": 5,
+		"stone": 4,
+		"wheat": 5,
+		"wood": 5,
+		"desert": 1,
+		"gold": 2,
+		"water": 16
+	},
+	"Seafarers: 5-6 Player": {		# 56 total, 30 with numbers
+		"brick": 5,
+		"sheep": 6,
+		"stone": 5,
+		"wheat": 6,
+		"wood": 6,
+		"desert": 2,
+		"gold": 2,
+		"water": 24
+	}
+}
+
 # Define the colors used for each tile
-all_tile_colors = {
+COLORS_PER_TILE = {
 	"brick": RGB((180, 60, 30)),
 	"sheep": RGB((20, 200, 50)),
 	"stone": RGB((127, 127, 127)),
