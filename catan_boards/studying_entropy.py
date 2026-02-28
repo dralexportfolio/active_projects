3024c439e7746448ec4bd38cfe1473f17e4d3e2f
dralexportfolio <dr.alex.portfolio@gmail.com>
2026-02-28 13:41:58 -0800

    Update studying_entropy.py

--- catan_boards/studying_entropy.py
+++ catan_boards/studying_entropy.py
@@ -25,7 +25,7 @@ from type_helper import tolerantlyCompare
 
 # External modules
 from math import log2, sqrt
-from numpy import random
+from numpy import quantile, random
 import plotly.graph_objects as go
 from tqdm import tqdm
 
@@ -37,17 +37,17 @@ from tqdm import tqdm
 reject_flag = False
 
 # Number of simulations to run and number of swaps to run per simulation
-N_SIMULATIONS = 10
-N_STEPS_PER_SIMULATION = 1000
+n_simulations = 10
+n_steps_per_simulation = 1000
 
 # Number of hexagons per side of board
-N_HEXAGONS_PER_SIDE = 5
+n_hexagons_per_side = 5
 
 # Set the names of the possible tile types
-ALL_TILE_TYPES = ["brick", "sheep", "stone", "wheat", "wood", "desert", "gold", "water"]
+all_tile_types = ["brick", "sheep", "stone", "wheat", "wood", "desert", "gold", "water"]
 
 # Tile types and likelihoods
-LIKELIHOOD_PER_TILE = {
+likelihood_per_tile = {
 	"brick": 0.1,
 	"sheep": 0.1,
 	"stone": 0.1,
@@ -58,20 +58,8 @@ LIKELIHOOD_PER_TILE = {
 	"water": 0.4
 }
 
-# Define the entropy weights by tile type
-ENTROPY_WEIGHT_BY_TILE = {
-	"brick": 1,
-	"sheep": 1,
-	"stone": 1,
-	"wheat": 1,
-	"wood": 1,
-	"desert": 1,
-	"gold": 1,
-	"water": 1
-}
-
 # Define the colors used for each tile
-COLORS_PER_TILE = {
+color_per_tile = {
 	"brick": RGB((180, 60, 30)),
 	"sheep": RGB((20, 200, 50)),
 	"stone": RGB((127, 127, 127)),
@@ -83,10 +71,10 @@ COLORS_PER_TILE = {
 }
 
 # Define the default bevel and sun information
-BEVEL_ATTITUDE = 25
-BEVEL_SIZE = 0.1
-SUN_ANGLE = 120
-SUN_ATTITUDE = 35
+bevel_attitude = 25
+bevel_size = 0.1
+sun_angle = 120
+sun_attitude = 35
 
 # Board rendering information
 debug_flag = False
@@ -98,10 +86,10 @@ dpi = 600
 ###############################################################################
 # Set the number of hexagons in each row of the board
 row_counts = []
-for index in range(N_HEXAGONS_PER_SIDE):
-	row_counts.append(N_HEXAGONS_PER_SIDE + index)
-for index in range(N_HEXAGONS_PER_SIDE - 1):
-	row_counts.append(2 * N_HEXAGONS_PER_SIDE - index - 2)
+for index in range(n_hexagons_per_side):
+	row_counts.append(n_hexagons_per_side + index)
+for index in range(n_hexagons_per_side - 1):
+	row_counts.append(2 * n_hexagons_per_side - index - 2)
 
 # Set the number of polygons based on the row counts
 n_polygons = sum(row_counts)
@@ -126,8 +114,8 @@ board = Board(n_polygons = n_polygons,
 			  y_shift_per_polygon = y_shift_per_polygon)
 
 # Apply the selected bevel and sun information to the board
-board.preprocessBevelInfo(bevel_attitude = BEVEL_ATTITUDE, bevel_size = BEVEL_SIZE)
-board.preprocessAllSunInfo(sun_angle = SUN_ANGLE, sun_attitude = SUN_ATTITUDE)
+board.preprocessBevelInfo(bevel_attitude = bevel_attitude, bevel_size = bevel_size)
+board.preprocessAllSunInfo(sun_angle = sun_angle, sun_attitude = sun_attitude)
 
 # Display the board for debugging (if needed)
 if debug_flag == True:
@@ -169,9 +157,9 @@ assert db_path is not None, "Unable to create db file because cancel button was 
 table_name = "sim_results"
 
 # Set the column names and types for this table
-column_names = ["sim_index", "step_index", "tile_type_1", "tile_type_2", "pre_total_weighted_entropy", "post_total_weighted_entropy", "delta_total_weighted_entropy"]
+column_names = ["sim_index", "step_index", "tile_type_1", "tile_type_2", "pre_mean_squared_error", "post_mean_squared_error", "delta_mean_squared_error"]
 column_types = ["BIGINT", "BIGINT", "TEXT", "TEXT", "FLOAT", "FLOAT", "FLOAT"]
-for tile_type in ALL_TILE_TYPES:
+for tile_type in all_tile_types:
 	column_names.append(tile_type + "_pre_efficiency")
 	column_types.append("FLOAT")
 
@@ -190,11 +178,14 @@ def computeEntropyInfo(needed_tile_types:list, tile_per_polygon:list) -> dict:
 	assert len(needed_tile_types) == len(set(needed_tile_types)), "computeEntropyInfo: Provided value for 'computeEntropyInfo' must be a list of distinct entries"
 	assert len(needed_tile_types) >= 2, "computeEntropyInfo: Provided value for 'computeEntropyInfo' must be a list with at least two entries"
 	for value in needed_tile_types:
-		assert value in ALL_TILE_TYPES, "computeEntropyInfo: Provided value for 'computeEntropyInfo' must be a list with entries that are valid tile types"
+		assert value in all_tile_types, "computeEntropyInfo: Provided value for 'computeEntropyInfo' must be a list with entries that are valid tile types"
 	# Tile types for each polygon
 	assert type(tile_per_polygon) == list, "computeEntropyInfo: Provided value for 'tile_per_polygon' must be a list object"
 	assert len(tile_per_polygon) == n_polygons, "computeEntropyInfo: Provided value for 'tile_per_polygon' must be a list of length equal to the number of polygons"
 	assert set(tile_per_polygon) == set(needed_tile_types), "computeEntropyInfo: Provided value for 'tile_per_polygon' must have exactly the entries contained in 'needed_tile_types'"
+
+	# Compute the theoretically maximum entropy
+	maximum_entropy = log2(len(needed_tile_types))
 
 	# Initialize the needed storage dictionaries
 	# Initialize the main dictionaries
@@ -236,12 +227,17 @@ def computeEntropyInfo(needed_tile_types:list, tile_per_polygon:list) -> dict:
 
 	# Compute the efficiency (i.e. normalized entropy) of each tile type's distribution
 	for tile_type in needed_tile_types:
-		efficiency_by_tile[tile_type] = entropy_by_tile[tile_type] / log2(len(needed_tile_types))
+		efficiency_by_tile[tile_type] = entropy_by_tile[tile_type] / maximum_entropy
 
-	# Compute the total weighted entropy using the needed values
-	total_weighted_entropy = 0
+	# Compute the mean squared error between the theoretical and actual entropy values
+	mean_squared_error = 0
 	for tile_type in needed_tile_types:
-		total_weighted_entropy += ENTROPY_WEIGHT_BY_TILE[tile_type] * entropy_by_tile[tile_type]
+		if tile_type == "water":
+			# Water should have low entropy
+			mean_squared_error += entropy_by_tile[tile_type]**2 / len(needed_tile_types)
+		else:
+			# All other tile types should have high entropy
+			mean_squared_error += (maximum_entropy - entropy_by_tile[tile_type])**2 / len(needed_tile_types)
 
 	# Construct the dictionary of results
 	all_entropy_results = {
@@ -249,25 +245,25 @@ def computeEntropyInfo(needed_tile_types:list, tile_per_polygon:list) -> dict:
 		"probability_results": probability_results,
 		"entropy_by_tile":  entropy_by_tile,
 		"efficiency_by_tile": efficiency_by_tile,
-		"total_weighted_entropy": total_weighted_entropy
+		"mean_squared_error": mean_squared_error
 	}
 
 	# Return the results
 	return all_entropy_results
 
 # Execute the simulation and write the needed information to the db file
-for sim_index in tqdm(range(N_SIMULATIONS)):
+for sim_index in tqdm(range(n_simulations)):
 	# Create a random initial tiling of the board using the probabilities
 	tile_per_polygon = []
 	for _ in range(n_polygons):
-		selected_tile_type = random.choice(a = list(LIKELIHOOD_PER_TILE.keys()), p = list(LIKELIHOOD_PER_TILE.values()))
+		selected_tile_type = random.choice(a = list(likelihood_per_tile.keys()), p = list(likelihood_per_tile.values()))
 		tile_per_polygon.append(selected_tile_type)
 
 	# Get the tile types which actually appear in this particular tiling
-	needed_tile_types = [tile_type for tile_type in ALL_TILE_TYPES if tile_type in tile_per_polygon]
+	needed_tile_types = [tile_type for tile_type in all_tile_types if tile_type in tile_per_polygon]
 
 	# Randomly swap tiles for the needed number of simulation steps
-	for step_index in range(N_STEPS_PER_SIMULATION):
+	for step_index in range(n_steps_per_simulation):
 		# Compute all relevant pre-swap entropy information
 		pre_all_entropy_results = computeEntropyInfo(needed_tile_types = needed_tile_types, tile_per_polygon = tile_per_polygon)
 
@@ -294,7 +290,7 @@ for sim_index in tqdm(range(N_SIMULATIONS)):
 		post_all_entropy_results = computeEntropyInfo(needed_tile_types = needed_tile_types, tile_per_polygon = tile_per_polygon)
 
 		# Undo the changes in the case of a bad swap(if needed)
-		if reject_flag == True and post_all_entropy_results["total_weighted_entropy"] < pre_all_entropy_results["total_weighted_entropy"]:
+		if reject_flag == True and post_all_entropy_results["mean_squared_error"] > pre_all_entropy_results["mean_squared_error"]:
 			tile_per_polygon[polygon_index_1] = tile_type_1
 			tile_per_polygon[polygon_index_2] = tile_type_2
 			post_all_entropy_results = pre_all_entropy_results
@@ -303,9 +299,9 @@ for sim_index in tqdm(range(N_SIMULATIONS)):
 		# Get the row index for the new row
 		row_index = getRowCount(db_path = db_path, table_name = table_name)
 		# Create a list containing the new row information
-		new_row = [sim_index, step_index, tile_type_1, tile_type_2, pre_all_entropy_results["total_weighted_entropy"], post_all_entropy_results["total_weighted_entropy"],
-				   post_all_entropy_results["total_weighted_entropy"] - pre_all_entropy_results["total_weighted_entropy"]]
-		for tile_type in ALL_TILE_TYPES:
+		new_row = [sim_index, step_index, tile_type_1, tile_type_2, pre_all_entropy_results["mean_squared_error"], post_all_entropy_results["mean_squared_error"],
+				   post_all_entropy_results["mean_squared_error"] - pre_all_entropy_results["mean_squared_error"]]
+		for tile_type in all_tile_types:
 			if tile_type in needed_tile_types:
 				new_row.append(pre_all_entropy_results["efficiency_by_tile"][tile_type])
 			else:
@@ -319,25 +315,37 @@ for sim_index in tqdm(range(N_SIMULATIONS)):
 ### Analyze the simulations and create relevant plots ###
 #########################################################
 # Initialize the needed storage
-all_delta_values = []
-all_efficiency_1_values = []
-all_efficiency_2_values = []
-all_color_values = []
+delta_values_by_quantile = {0: [], 1: [], 2: [], 3: []}
+efficiency_1_values_by_quantile = {0: [], 1: [], 2: [], 3: []}
+efficiency_2_values_by_quantile = {0: [], 1: [], 2: [], 3: []}
 
 # Compute the maximum magnitude delta value in the db file
-delta_column = readColumn(db_path = db_path, table_name = table_name, column_name = "delta_total_weighted_entropy")
+delta_column = readColumn(db_path = db_path, table_name = table_name, column_name = "delta_mean_squared_error")
 max_abs_delta = max(abs(max(delta_column)), abs(min(delta_column)))
+
+# Get the quantile information from the delta column
+quantile_values = quantile(a = delta_column, q = [0, 0.25, 0.5, 0.75, 1])
 
 # Get the total number of rows from the db file
 n_rows = getRowCount(db_path = db_path, table_name = table_name)
 
 # Read the needed information from the table
 for row_index in range(n_rows):
-	# Get the change in total weighted entropy associated with this swap
-	delta_total_weighted_entropy = readEntry(db_path = db_path, table_name = table_name, column_name = "delta_total_weighted_entropy", row_index = row_index)
+	# Get the change in mean squared error associated with this swap
+	delta_mean_squared_error = readEntry(db_path = db_path, table_name = table_name, column_name = "delta_mean_squared_error", row_index = row_index)
+
+	# Determine to which quantile the relevant data should be added
+	if quantile_values[0] <= delta_mean_squared_error and delta_mean_squared_error < quantile_values[1]:
+		quantile_index = 0
+	elif quantile_values[1] <= delta_mean_squared_error and delta_mean_squared_error <= quantile_values[2]:
+		quantile_index = 1
+	elif quantile_values[2] <= delta_mean_squared_error and delta_mean_squared_error <= quantile_values[3]:
+		quantile_index = 2
+	else:
+		quantile_index = 3
 
 	# Proceed with the analysis (if needed)
-	if reject_flag == False or delta_total_weighted_entropy > 0:
+	if reject_flag == False or delta_mean_squared_error < 0:
 		# Get the two tile types involved with this swap
 		tile_type_1 = readEntry(db_path = db_path, table_name = table_name, column_name = "tile_type_1", row_index = row_index)
 		tile_type_2 = readEntry(db_path = db_path, table_name = table_name, column_name = "tile_type_2", row_index = row_index)
@@ -347,14 +355,14 @@ for row_index in range(n_rows):
 		efficiency_2 = readEntry(db_path = db_path, table_name = table_name, column_name = tile_type_2 + "_pre_efficiency", row_index = row_index)
 
 		# Append to the needed lists
-		all_delta_values.append(delta_total_weighted_entropy)
-		all_efficiency_1_values.append(efficiency_1)
-		all_efficiency_2_values.append(efficiency_2)
+		delta_values_by_quantile[quantile_index].append(delta_mean_squared_error)
+		efficiency_1_values_by_quantile[quantile_index].append(efficiency_1)
+		efficiency_2_values_by_quantile[quantile_index].append(efficiency_2)
 
 # Set the color scale and color bound values as needed
 if reject_flag == False:
 	rgb_spectrum = ALL_PLOTLY_COLOR_SCALES_BY_TYPE["diverging"]["Portland"]
-	color_function = lambda index: customSpectrum(parameter = 1 - index / 100, rgb_spectrum = rgb_spectrum)
+	color_function = lambda index: customSpectrum(parameter = index / 100, rgb_spectrum = rgb_spectrum)
 	color_scale = [[index / 100, color_function(index).asStringTuple()] for index in range(101)]
 	c_min = -max_abs_delta
 	c_max = max_abs_delta
@@ -368,23 +376,28 @@ else:
 # Create a scatter plot showing how efficiency values affect delta values
 # Create the figure
 fig = go.Figure()
+# Set the trace names
+trace_name_by_quantile = ["Great Swaps", "Good Swaps", "Bad Swaps", "Terrible Swaps"]
 # Add the needed traces
-fig.add_trace(go.Scatter(x = all_efficiency_1_values,
-						 y = all_efficiency_2_values,
-						 showlegend = False,
-						 customdata = all_delta_values,
-						 hovertemplate = ("<b>Pre-Swap Efficiency 1:</b> %{x}<br>"
-										  "<b>Pre-Swap Efficiency 2:</b> %{y}<br>"
-										  "<b>Delta Total Weighted Entropy:</b> %{customdata}<br>"
-										  "<extra></extra>"),
-						 mode = "markers",
-						 marker = {"color": all_delta_values,
-								   "colorscale": color_scale,
-								   "showscale": True,
-								   "cmin": c_min,
-								   "cmax": c_max}))
+for quantile_index in range(4):
+	fig.add_trace(go.Scatter(x = efficiency_1_values_by_quantile[quantile_index],
+							 y = efficiency_2_values_by_quantile[quantile_index],
+							 name = trace_name_by_quantile[quantile_index],
+							 showlegend = True,
+							 customdata = delta_values_by_quantile[quantile_index],
+							 hovertemplate = ("<b>Pre-Swap Efficiency 1:</b> %{x}<br>"
+											  "<b>Pre-Swap Efficiency 2:</b> %{y}<br>"
+											  "<b>Delta Mean Squared Error:</b> %{customdata}<br>"
+											  "<extra></extra>"),
+							 mode = "markers",
+							 marker = {"color": delta_values_by_quantile[quantile_index],
+									   "colorscale": color_scale,
+									   "showscale": False,
+									   "cmin": c_min,
+									   "cmax": c_max}))
 # Format the figure
-fig.update_layout(title = "Deltas In Total Weighted Entropy As Functions Of Efficiency Values")
+fig.update_layout(title = "Deltas In Mean Squared Error As Functions Of Efficiency Values",
+				  xaxis = {"range": [-0.05, 1.05]}, yaxis = {"range": [-0.05, 1.05]})
 fig.update_xaxes(title = "pre-swap efficiency 1")
 fig.update_yaxes(title = "pre-swap efficiency 2")
 # Show the figure
